b703340b45570e57cbef146d67598c665702cc58
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-04-13 19:31:28 +0300

    lol

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  let emoji = "";
+
+  if (minutes < 30) {
+    emoji = "☕️".repeat(Math.ceil(minutes / 5));
+  } else {
+    emoji = "🍱".repeat(Math.ceil(minutes / 10));
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{emoji} {minutes} min read • {date}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
